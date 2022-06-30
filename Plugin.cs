2f3ee86ef468f1b10fe293a02c9092b95b204b13
ARandomGitHubAccount <107531619+ARandomGitHubAccount@users.noreply.github.com>
2022-06-30 11:39:01 +0200

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -14,7 +14,7 @@ namespace GatheringSkill
     {
         internal const string ModName = "GatheringSkill";
         internal const string ModVersion = "1.0.0";
-        internal const string Author = "grisch";
+        internal const string Author = "ARGA";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
         private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
